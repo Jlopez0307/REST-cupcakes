7a5884ae2fcc347c04f5d1d65ca949d9cf2d3dad
Jlopez0307 <55158803+Jlopez0307@users.noreply.github.com>
2023-02-06 00:07:16 -0500

    Added routes for listing cupcakes and getting one cupcake by id. Started writing route for creating cupcake

--- app.py
+++ app.py
@@ -1,2 +1,32 @@
 """Flask app for Cupcakes"""
+from flask import Flask, request, render_template, redirect, jsonify
+from models import db, connect_db, Cupcake
+
+
+app = Flask(__name__)
+app.app_context().push()
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:Halo03117!@localhost:5432/cupcakes'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
+app.config['SQLALCHEMY_ECHO'] = True
+connect_db(app)
+
+
+app.config['SECRET_KEY'] = 'cats'
+app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = False
+
+@app.route('/api/cupcakes', methods = ["GET", "POST"])
+def list_cupcakes():
+    if request.method == "POST":
+        new_cupcake = Cupcake()
+
+    all_cupcakes = [cupcake.serialize() for cupcake in Cupcake.query.all()]
+    return jsonify(cupcakes = all_cupcakes)
+
+@app.route('/api/cupcakes/<int:cupcake_id>')
+def get_cupcake(cupcake_id):
+    cupcake = Cupcake.query.get_or_404(cupcake_id)
+    return jsonify(cupcake = cupcake.serialize())
+
+
 
--- models.py
+++ models.py
@@ -1,1 +1,34 @@
 """Models for Cupcake app."""
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+def connect_db(app):
+    db.app = app
+    db.init_app(app)
+
+class Cupcake(db.Model):
+    __tablename__ = "cupcakes"
+
+    def __repr__(self):
+        c = self
+        return f""
+
+    def serialize(self):
+        return {
+            'id': self.id,
+            'flavor': self.flavor,
+            'size': self.size,
+            'rating': self.rating,
+            'image': self.image
+        }
+
+    id = db.Column( db.Integer, primary_key=True, autoincrement=True )
+    flavor = db.Column( db.String(50), nullable = False )
+    size = db.Column( db.String(50), nullable = False)
+    rating = db.Column( db.Float, nullable = False)
+    image = db.Column( db.Text, nullable = False, server_default = "https://tinyurl.com/demo-cupcake")
+
+    def image_url(self):
+        return self.image or "https://tinyurl.com/demo-cupcake"
+
+    
